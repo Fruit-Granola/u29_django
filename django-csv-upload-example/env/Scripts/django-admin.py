b3fc452be7cdfab784a9e362d3c4ad7695e803dc
Fruit_Granola <wata11searching@gmail.com>
2020-12-27 22:23:30 +0900

    organize files

---
+++ django-csv-upload-example/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\amil\u29_django\django-csv-upload-example\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
