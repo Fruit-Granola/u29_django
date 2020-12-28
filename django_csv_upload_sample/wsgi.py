c04dc6be667322f814d1096fc7d21627be0269d3
Fruit_Granola <wata11searching@gmail.com>
2020-12-28 13:37:24 +0900

    modify wsgi settinigs

--- django_csv_upload_sample/wsgi.py
+++ django_csv_upload_sample/wsgi.py
@@ -10,14 +10,8 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_csv_upload_sample.settings')
-
-application = get_wsgi_application()
-
-from dj_static import Cling
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sample_app.settings")
 
 application = Cling(get_wsgi_application())
